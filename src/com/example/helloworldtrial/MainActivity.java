1d95bbc957222b8190cc76058f021ed002b6cecd
Aditya Bansal <aditya.bansal93@gmail.com>
2014-03-31 19:20:55 -0700

    Added gesture Detection - Tapping works :)

--- src/com/example/helloworldtrial/MainActivity.java
+++ src/com/example/helloworldtrial/MainActivity.java
@@ -1,23 +1,87 @@
 package com.example.helloworldtrial;
 
-import com.google.android.glass.app.Card;
-
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+
+import com.google.android.glass.app.Card;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 
 public class MainActivity extends Activity {
 
+	private GestureDetector mGestureDetector;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
+		
 		Card card = new Card(getApplicationContext());
 		card.setText("wqdwq");
 		card.setFootnote("well hello there");
 		View view = card.toView();
 		setContentView(view);
 	
+		mGestureDetector = createGestureDetector(this);
 	}
+	
+	private GestureDetector createGestureDetector(Context context) {
+	    GestureDetector gestureDetector = new GestureDetector(context);
+	        //Create a base listener for generic gestures
+	        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+	            @Override
+	            public boolean onGesture(Gesture gesture) {
+	                if (gesture == Gesture.TAP) {
+	                	Card card = new Card(getApplicationContext());
+	            		card.setText("Tapped!");
+	            		card.setFootnote("well hello there");
+	            		View view = card.toView();
+	            		setContentView(view);
+	                    return true;
+	                } else if (gesture == Gesture.TWO_TAP) {
+	                    // do something on two finger tap
+	                    return true;
+	                } else if (gesture == Gesture.SWIPE_RIGHT) {
+	                    // do something on right (forward) swipe
+	                    return true;
+	                } else if (gesture == Gesture.SWIPE_LEFT) {
+	                    // do something on left (backwards) swipe
+	                    return true;
+	                }
+	                return false;
+	            }
+	        });
+	        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+	            @Override
+	            public void onFingerCountChanged(int previousCount, int currentCount) {
+	              // do something on finger count changes
+	            }
+	        });
+	        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+	            @Override
+	            public boolean onScroll(float displacement, float delta, float velocity) {
+					return false;
+	                // do something on scrolling
+	            }
+	        });
+	        return gestureDetector;
+	    }
+
+	    /*
+	     * Send generic motion events to the gesture detector
+	     */
+	    @Override
+	    public boolean onGenericMotionEvent(MotionEvent event) {
+	        if (mGestureDetector != null) {
+	            return mGestureDetector.onMotionEvent(event);
+	        }
+	        return false;
+	    }
+
 
 
 
